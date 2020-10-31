0cbf5f8d5fa8073a122d8515f5b104a206099c03
jrshort89 <jrshort89@gmail.com>
2020-10-30 21:23:55 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
+  get 'genres/show'
+
+  get 'genres/new'
+
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/new'
+
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
